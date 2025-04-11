837de25a4f55fc9cba175ab12d2822510408792f
voidKandy <voidkandy@gmail.com>
2025-04-11 10:26:30 -0700

    mesh and physics aligned

--- src/main.zig
+++ src/main.zig
@@ -15,6 +15,12 @@ fn init_camera() rl.Camera3D {
     return camera;
 }
 
+/// Helper function to translate raylib transforms to physics engine transforms
+fn vec3_arr(vec: Vector3) *const [3]f32 {
+    const arr = [_]f32{ vec.x, vec.y, vec.z };
+    return &arr;
+}
+
 pub fn main() anyerror!void {
     // Initialization
     //--------------------------------------------------------------------------------------
@@ -22,6 +28,12 @@ pub fn main() anyerror!void {
     const screenHeight = 450;
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
+
+    defer {
+        const deinit_status = gpa.deinit();
+        //fail test; can't try in defer as defer is executed after we return
+        if (deinit_status == .leak) std.testing.expect(false) catch @panic("TEST FAIL");
+    }
 
     zbt.init(allocator);
     const world = zbt.initWorld();
@@ -32,8 +44,26 @@ pub fn main() anyerror!void {
 
     const camera = init_camera();
 
-    const cube_pos = Vector3.init(0.0, 1.0, 0.0);
+    var cube_pos = Vector3.init(0.0, 1.0, 0.0);
     const cube_size = Vector3.init(2.0, 2.0, 2.0);
+
+    const physics_box_shape = zbt.initBoxShape(vec3_arr(cube_size));
+    defer physics_box_shape.deinit();
+    // Create rigid body that will use above shape.
+    const initial_transform = [_]f32{
+        1.0, 0.0, 0.0, // orientation
+        0.0, 1.0, 0.0,
+        0.0, 0.0, 1.0,
+        0.0, 1.0, 0.0, // translation SAME AS `cube_pos`
+    };
+    const box_body = zbt.initBody(
+        1.0, // mass (0.0 for static objects)
+        &initial_transform,
+        physics_box_shape.asShape(),
+    );
+    defer box_body.deinit();
+    world.addBody(box_body);
+    defer world.removeBody(box_body);
 
     rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
     defer rl.closeWindow(); // Close window and OpenGL context
@@ -42,10 +72,20 @@ pub fn main() anyerror!void {
     //--------------------------------------------------------------------------------------
 
     // Main game loop
-    while (!rl.windowShouldClose()) { // Detect window close button or ESC key
+    while (!rl.windowShouldClose()) {
+        const dt = rl.getFrameTime();
+        // Detect window close button or ESC key
         // Update
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
+        _ = world.stepSimulation(dt, .{});
+
+        const cube_body = world.getBody(0);
+        var transform: [12]f32 = undefined;
+        cube_body.getGraphicsWorldTransform(&transform);
+        cube_pos.x = transform[9];
+        cube_pos.y = transform[10];
+        cube_pos.z = transform[11];
+
         //----------------------------------------------------------------------------------
 
         // Draw
@@ -79,5 +119,7 @@ pub fn main() anyerror!void {
         rl.drawText("Right click mouse to toggle camera controls", 10, 430, 10, rl.Color.gray);
 
         rl.drawFPS(10, 10);
+
+        //
     }
 }
