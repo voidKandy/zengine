f7dab21acb1820e2f743a871c1b3b83f2e497da0
voidKandy <voidkandy@gmail.com>
2025-05-22 12:00:50 -0700

    fixed issue

--- engine/root.zig
+++ engine/root.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 pub const da = @import("dynamic_array.zig");
 pub const ecs = @import("ecs.zig");
-pub const cm = @import("composite_mesh.zig");
 pub const util = @import("util.zig");
 pub const MeshBundle = @import("MeshBundle.zig");
 
